bbd290d33f37a02a972baa493eace2f1f1521dc2
lwattendorf <lauren.wattendorf@gmail.com>
2018-02-28 11:22:42 -0800

    Moved podfile for firebase set up, named all the view controllers, and created two new view controller files for login

---
+++ TriHealth/TriHealth/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  TriHealth
+//
+//  Created by Wattendorf Laptop on 2/28/18.
+//  Copyright © 2018 Lucy Ackerman. All rights reserved.
+//
+
+import UIKit
+
+class ProfielViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ TriHealth/TriHealth/SignInViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignInViewController.swift
+//  TriHealth
+//
+//  Created by Wattendorf Laptop on 2/28/18.
+//  Copyright © 2018 Lucy Ackerman. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignInViewController: UIViewController {
+    
+    //variables
+    let databaseref = FIRDatabase.database().reference(fromURL: "https://trihealth-d669c.firebaseio.com/")
+    //outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //actions
+    
+    //func
+}
